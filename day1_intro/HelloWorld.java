e3cb840f2810096e1fd2a96b48649ba5ae75b64f
Hendrix Tavarez <htavarez@gmail.com>
2025-08-26 19:35:21 -0400

    intro

--- day1_intro/HelloWorld.java
+++ day1_intro/HelloWorld.java
@@ -1,10 +1,6 @@
-// add package name here if needed
-//
 class HelloWorld {
-
-  public static void main(String[] args) {
-    System.out.println("Hello, world!");
-    
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
 
