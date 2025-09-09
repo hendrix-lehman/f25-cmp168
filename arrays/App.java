a8faed25b1c4cb30fd44a25746a7dd3f850fade1
Hendrix Tavarez <htavarez@gmail.com>
2025-09-09 18:51:38 -0400

    sep 9

--- arrays/App.java
+++ arrays/App.java
@@ -1,7 +1,6 @@
 // add package name here if needed
 //
 class App {
-
   public static void main(String[] args) {
 
     // Array of integers
@@ -34,6 +33,7 @@ class App {
       System.out.println("array values[" + i + "] = " + values[i]);
     }
 
+//  swap first and last values
     int temp = values[0];
     values[0] = values[4];
     values[4] = temp;
@@ -41,6 +41,83 @@ class App {
     for (int i = 0; i < values.length; i++) {
       System.out.println("array values[" + i + "] = " + values[i]);
     } 
+
+    // how to copy an array
+    int[] copy = new int[values.length];
+    for (int i = 0; i < values.length; i++) {
+      copy[i] = values[i];
+    }
+
+    // resize an array
+    int[] resized = new int[values.length * 2];
+    for (int i = 0; i < values.length; i++) {
+      resized[i] = values[i];
+    }
+    values = resized;
+    System.out.println("After resizing:");
+    for (int i = 0; i < values.length; i++) {
+      System.out.println("array values[" + i + "] = " + values[i]);
+    }
+    
+    // double the size of an array using a method
+    int[] newValues = { -121, 26, 3, 144, 50 };
+    // int[] returnedFromMethod = doubleSizeOfArray(newValues);
+    newValues = doubleSizeOfArray(newValues);
+    System.out.println("1.After attempting to double size in method:");
+    System.out.println("1.array newValues length: " + newValues.length);
+    System.out.println("1.array newValues contents:");
+    printArray(newValues);
+
+    System.out.println("Integer MIN_VALUE: " + Integer.MIN_VALUE);
+    int max = getMaxOfArray(newValues);
+    System.out.println("Max value in newValues array: " + max); 
+    int min = getMinOfArray(newValues);
+    System.out.println("Min value in newValues array: " + min);
+
   }
+
+  public static void printArray(int[] array) {
+    for (int i = 0; i < array.length; i++) {
+      System.out.println("array[" + i + "] = " + array[i]);
+    }
+  }
+
+  public static int getMaxOfArray(int[] array) {
+    int max = Integer.MIN_VALUE;
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] > max) {
+        max = array[i];
+      }
+    }
+    return max;
+  }
+
+  public static int getMinOfArray(int[] array) {
+    int min = Integer.MAX_VALUE;
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
+      }
+    }
+    return min;
+  }
+
+  public static int[] doubleSizeOfArray(int[] array) {
+    // println("Inside method, original length: " + array.length);
+    printArray(array);
+
+    // double the size of the array and copy the values
+    int[] newArray = new int[array.length * 2];
+    for (int i = 0; i < array.length; i++) {
+      newArray[i] = array[i];
+    }
+    array = newArray;
+    System.out.println("Inside method, new length: " + array.length);
+    printArray(array);
+    return array;
+    
+  }
+
+
 }
 
