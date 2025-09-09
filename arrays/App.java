6fe87fa49d2635e86ee8c46333a631535823ef29
Hendrix Tavarez <htavarez@gmail.com>
2025-09-09 19:52:46 -0400

    sep 9

--- arrays/App.java
+++ arrays/App.java
@@ -68,11 +68,37 @@ class App {
     System.out.println("1.array newValues contents:");
     printArray(newValues);
 
+    System.out.println("Integer MAX_VALUE: " + Integer.MAX_VALUE);
     System.out.println("Integer MIN_VALUE: " + Integer.MIN_VALUE);
     int max = getMaxOfArray(newValues);
     System.out.println("Max value in newValues array: " + max); 
     int min = getMinOfArray(newValues);
     System.out.println("Min value in newValues array: " + min);
+
+    int sum = sumOfArray(newValues);
+    System.out.println("Sum of values in newValues array: " + sum);
+
+    double mean = meanOfArray(newValues);
+    System.out.println("Mean of values in newValues array: " + mean);
+
+    int searchFor = 144;
+    // int index = linearSearch(newValues, searchFor);
+    int[] sortedForSearch = insertionSort(newValues);
+    printArray(sortedForSearch);
+    int index = binarySearch(sortedForSearch, searchFor);
+    if (index != -1) {
+      System.out.println("Found " + searchFor + " at index " + index);
+    } else {
+      System.out.println(searchFor + " not found in the array");
+    }
+
+    // System.out.println("Before sorting:");
+    // printArray(newValues);
+    // int[] sorted = bubbleSort(newValues);
+    // int[] sorted = selectionSort(newValues);
+    // int[] sorted = insertionSort(newValues);
+    // System.out.println("After sorting:");
+    // printArray(sorted);
 
   }
 
@@ -80,6 +106,102 @@ class App {
     for (int i = 0; i < array.length; i++) {
       System.out.println("array[" + i + "] = " + array[i]);
     }
+  }
+
+  public static int linearSearch(int[] array, int value) {
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == value) {
+        return i;
+      }
+    }
+    return -1; // not found
+  }
+
+  public static int binarySearch(int[] array, int value) {
+    int left = 0;
+    int right = array.length - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (array[mid] == value) {
+        return mid;
+      }
+      if (array[mid] < value) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return -1; // not found
+  }
+
+  public static int[] insertionSort(int[] array) {
+    int n = array.length;
+    for (int i = 1; i < n; i++) {
+      int key = array[i];
+      int j = i - 1;
+
+      // Move elements of array[0..i-1], that are greater than key,
+      // to one position ahead of their current position
+      while (j >= 0 && array[j] > key) {
+        array[j + 1] = array[j];
+        j = j - 1;
+      }
+      array[j + 1] = key;
+    }
+    return array;
+  }
+
+  public static int[] selectionSort(int[] array) {
+    int n = array.length;
+    for (int i = 0; i < n - 1; i++) {
+      // Find the minimum element in unsorted array
+      int minIdx = i;
+      for (int j = i + 1; j < n; j++) {
+        if (array[j] < array[minIdx]) {
+          minIdx = j;
+        }
+      }
+      // Swap the found minimum element with the first element
+      int temp = array[minIdx];
+      array[minIdx] = array[i];
+      array[i] = temp;
+    }
+    return array;
+  }
+
+  public static int[] bubbleSort(int[] array) {
+    int n = array.length;
+    boolean swapped;
+    for (int i = 0; i < n - 1; i++) {
+      swapped = false;
+      for (int j = 0; j < n - i - 1; j++) {
+        if (array[j] > array[j + 1]) {
+          // swap array[j] and array[j+1]
+          int temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+          swapped = true;
+        }
+      }
+      // If no two elements were swapped by inner loop, then break
+      if (!swapped) {
+        break;
+      }
+    }
+    return array;
+  }
+
+  public static double meanOfArray(int[] array) {
+    int sum = sumOfArray(array); 
+    return (double) sum / array.length;
+  }
+
+  public static int sumOfArray(int[] array) {
+    int sum = 0;
+    for (int i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum;
   }
 
   public static int getMaxOfArray(int[] array) {
